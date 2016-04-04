51aaf0577d62b66de07bb0539cb60bae50b6dbfe
Charlon Palacay <charlon@gmail.com>
2016-04-04 14:49:52 -0700

    Removes Pods directory from repo.

--- Pods/Target Support Files/Pods-ScoutUITests/Pods-ScoutUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ScoutUITests : NSObject
-@end
-@implementation PodsDummy_Pods_ScoutUITests
-@end
--- Pods/Target Support Files/Turbolinks/Turbolinks-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double TurbolinksVersionNumber;
-FOUNDATION_EXPORT const unsigned char TurbolinksVersionString[];
-
